17c4b86aab942136dca5b2df0ed3379a202c2851
kian <wangjian@magicmoma.com>
2015-09-18 23:41:08 +0800

    Evaluate

--- Chapter-1/Evaluate.java
+++ Chapter-1/Evaluate.java
@@ -3,6 +3,12 @@ import java.util.Stack;
 public class Evaluate {
     public static void main(String[] args) {
         Stack<String> ops = new Stack<String>();
-        System.out.println("yes:");
+        Stack<Double> vals = new Stack<Double>();
+        while (StdIn.isEmpty()) {
+            String str = StdIn.readString();
+            if (str.equals("(")) ;
+            else if (str.equals("+")) ops.push(str);
+            System.out.println("y: " + ops.pop());
+        }
     }
 }
