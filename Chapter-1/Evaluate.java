23581b0b7507f95f2b36cbdceff522ca6bd47061
kian <wangjian@magicmoma.com>
2015-11-04 14:42:22 +0800

    intellij idea

--- Chapter-1/Evaluate.java
+++ Chapter-1/Evaluate.java
@@ -4,7 +4,7 @@ public class Evaluate {
     public static void main(String[] args) {
         Stack<String> ops = new Stack<String>();
         Stack<Double> vals = new Stack<Double>();
-        while (StdIn.isEmpty()) {
+        while (args.length == 0) {
             String str = StdIn.readString();
             if (str.equals("(")) ;
             else if (str.equals("+")) ops.push(str);
