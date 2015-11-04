0813cb03f248629a61f45dc7ef11358df8ae6d66
kian <wangjian@magicmoma.com>
2015-11-04 18:01:50 +0800

    continue

--- Chapter-1/Evaluate.java
+++ Chapter-1/Evaluate.java
@@ -1,14 +1,27 @@
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Evaluate {
     public static void main(String[] args) {
         Stack<String> ops = new Stack<String>();
         Stack<Double> vals = new Stack<Double>();
-        while (args.length == 0) {
-            String str = StdIn.readString();
-            if (str.equals("(")) ;
-            else if (str.equals("+")) ops.push(str);
-            System.out.println("y: " + ops.pop());
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String str = scanner.next();
+
+            if ("(".equals(str)) ;
+            else if ("+".equals(str)) ops.push(str);
+            else if (")".equals(str)) {
+                String op = ops.pop();
+                double v = vals.pop();
+
+                if (op.equals("+")) vals.push(vals.pop() + v);
+
+            }
+
+            else vals.push(Double.parseDouble(str));
         }
     }
 }
