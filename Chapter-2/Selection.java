f6e42533327bd40d872b89885c64ca54c26b8e65
kian <kian@example.com>
2016-06-24 11:27:55 +0800

    modify Selection Sort

--- Chapter-2/Selection.java
+++ Chapter-2/Selection.java
@@ -8,19 +8,20 @@ import java.util.Arrays;
 
 public class Selection extends Sort{
     public static void main(String[] args) {
-        int[] arr = {4, 7, 6, 3, 1, 8, 5};
+        Comparable[] arr = {4, 7, 6, 3, 1, 8, 5};
         sort(arr);
     }
    
-    public static void sort(int[] arr) {
+    public static void sort(Comparable[] arr) {
 		int min;
         for (int i=0; i<arr.length; i++) {
-		    min = arr[i];
+		    min = i;
 			for (int j=i+1; j<arr.length; j++) {
-			    if (less(arr, i, j) > 0) {
-				    exch(arr, i, j);
+			    if (less(arr[j], arr[min])) {
+				    min = j;
 				}
 			}
+			exch(arr, i, min);
 			System.out.println(Arrays.toString(arr));
         }        
     }
--- Chapter-2/Sort.java
+++ Chapter-2/Sort.java
@@ -1,12 +1,14 @@
 public class Sort {
-    public static int less(int[] arr, int i, int j) {
-	    if (arr[i] < arr[j]) return -1;
-		if (arr[i] > arr[j]) return +1;
-		return 0;
+    /**
+	* 比较两个元素大小
+	*/
+    public static boolean less(Comparable t, Comparable v) {
+	    return t.compareTo(v) < 0;
 	}
 
-	public static void exch(int[] arr, int i, int j) {
-	    int tmp = arr[i];
+	public static void exch(Comparable[] arr, int i, int j) {
+	    System.err.println("交换");
+	    Comparable tmp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = tmp;
 	}
